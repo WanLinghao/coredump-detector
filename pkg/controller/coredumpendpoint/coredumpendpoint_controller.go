ed1adfc7c105b0d8678da775ce5b5394718b68d5
WanLinghao <wanlh.fnst@cn.fujitsu.com>
2019-05-30 09:46:34 +0800

    Execute 'apiserver-boot create resource --group=coredump --version=v1alpha1 --kind=CoredumpEndpoint'

---
+++ pkg/controller/coredumpendpoint/coredumpendpoint_controller.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package coredumpendpoint
+
+import (
+	"context"
+
+	coredumpv1alpha1 "github.com/WanLinghao/fujitsu-coredump/pkg/apis/coredump/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new CoredumpEndpoint Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileCoredumpEndpoint{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("coredumpendpoint-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to CoredumpEndpoint
+	err = c.Watch(&source.Kind{Type: &coredumpv1alpha1.CoredumpEndpoint{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create
+	// Uncomment watch a Deployment created by CoredumpEndpoint - change this for objects you create
+	// err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+	//	IsController: true,
+	//	OwnerType:    &coredumpv1alpha1.CoredumpEndpoint{},
+	// })
+	// if err != nil {
+	//	return err
+	// }
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileCoredumpEndpoint{}
+
+// ReconcileCoredumpEndpoint reconciles a CoredumpEndpoint object
+type ReconcileCoredumpEndpoint struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a CoredumpEndpoint object and makes changes based on the state read
+// and what is in the CoredumpEndpoint.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
+// a Deployment as an example
+// +kubebuilder:rbac:groups=coredump.fujitsu.com,resources=coredumpendpoints,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=coredump.fujitsu.com,resources=coredumpendpoints/status,verbs=get;update;patch
+func (r *ReconcileCoredumpEndpoint) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the CoredumpEndpoint instance
+	instance := &coredumpv1alpha1.CoredumpEndpoint{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
